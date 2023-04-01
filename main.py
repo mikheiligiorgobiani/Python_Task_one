fc989eb1206b9f477dd968823adb192ccfa98dc7
mikheiligiorgobiani <mikheiligiorgobiani@gmail.com>
2023-04-02 00:43:19 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# task one
+
+name = "misho"
+age = 21
+print(f'{name} is {age} years old ')
+
+# task two
+
+country = "Georgia's"
+population = 3.5
+print(f'{country.title()} population is {population} million ')
